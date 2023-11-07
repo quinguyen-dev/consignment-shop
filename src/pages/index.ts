fcaf4309d41cd5d7717b105610c2e0b334b439f3
Ian W <49083526+Landaman@users.noreply.github.com>
2023-11-07 18:32:01 -0500

    Merge

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,4 +2,4 @@ export { InventoryManager } from "./InventoryManager/InventoryManager";
 export { StoreOwnerDashboard } from "./StoreOwner/StoreOwnerDashboard.tsx";
 export { Landing } from "./Landing/Landing";
 export { Login } from "./Login/Login";
-export { SiteManagerDashboard } from "./SiteManager/SiteManagerDashboard.tsx";
+export { default as SiteManagerDashboard } from "./SiteManager/SiteManagerDashboard.tsx";
