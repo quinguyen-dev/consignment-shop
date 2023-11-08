5dbe67b937679a1fb5f3d6d4829b4c5f7a1e6ab1
Ian W <49083526+Landaman@users.noreply.github.com>
2023-11-07 23:34:27 -0500

    Delete modal

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App.tsx";
+import Modal from "react-modal";
+
 
 const queryClient = new QueryClient();
+Modal.setAppElement('#root');
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
--- src/pages/SiteManager/SiteManagerDashboard.tsx
+++ src/pages/SiteManager/SiteManagerDashboard.tsx
@@ -1,62 +1,14 @@
 import placeholder from "@/assets/placeholder.png";
 import { NavigationCard } from "@/components";
 import { useSiteManagerData } from "@/hooks/useSiteManagerData.ts";
-import {useMemo, useState} from "react";
-import {
-  createColumnHelper,
-  flexRender,
-  getCoreRowModel,
-  useReactTable,
-} from "@tanstack/react-table";
-import { Store } from "@/hooks/types.ts";
-import dataIcon from "@/assets/data.svg";
-import trashIcon from "@/assets/trash.svg";
+import {useState} from "react";
+import StoresDataTable from "@/pages/SiteManager/StoresDataTable.tsx";
 
 function SiteManagerDashboard() {
   const [manageMode, setManageMode] = useState(false);
 
   const managerData = useSiteManagerData();
   const query = managerData.fetchAll();
-
-  const columns = useMemo(() => {
-    const helper = createColumnHelper<Store>();
-    return [
-      helper.accessor("storeId", { header: "#" }),
-      helper.accessor("storeName", { header: "Store Name" }),
-      helper.accessor(
-        (item) => {
-          return `$${item.balance.toLocaleString()}`;
-        },
-        { header: "Balance" },
-      ),
-      helper.display({
-        header: "",
-        id: "viewDataColumn",
-        cell: (_) => (
-          <button>
-            <img src={dataIcon} alt="View Data"></img>
-          </button>
-        ),
-      }),
-      helper.display({
-        header: "",
-        id: "trashColumn",
-        cell: (props) => (
-          <button onClick={() => {
-            managerData.remove(props.getValue<Store>().storeId)
-          }}>
-            <img src={trashIcon} alt="Delete Store"></img>
-          </button>
-        ),
-      }),
-    ];
-  }, []);
-
-  const table = useReactTable({
-    data: query.data?.stores ?? [],
-    columns,
-    getCoreRowModel: getCoreRowModel(),
-  });
 
   return (
     <div className="mt-8 mx-4">
@@ -102,40 +54,7 @@ function SiteManagerDashboard() {
             selected={manageMode}
           />
         </div>
-        {!manageMode &&
-        <table className="mt-4 flex-grow">
-          <thead>
-            {table.getHeaderGroups().map((group) => (
-              <tr key={group.id} className="border-b-2">
-                {group.headers.map((header) => (
-                  <th
-                    key={header.id}
-                    className="py-2 px-4 text-sm font-bold text-gray-900 text-left"
-                  >
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(
-                          header.column.columnDef.header,
-                          header.getContext(),
-                        )}
-                  </th>
-                ))}
-              </tr>
-            ))}
-          </thead>
-          <tbody>
-            {table.getRowModel().rows.map((row) => (
-              <tr key={row.id} className="border-b-[1px]">
-                {row.getVisibleCells().map((cell) => (
-                  <td key={cell.id} className="px-4 py-2">
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </td>
-                ))}
-              </tr>
-            ))}
-          </tbody>
-        </table>
-        }
+        {!manageMode && <div className="flex-grow"><StoresDataTable/></div>}
       </div>
     </div>
   );
