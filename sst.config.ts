316e141e87daf3ee50e42726c6095b7451e0962a
Michael O'Connor <michaeloconnor1717@gmail.com>
2023-11-15 17:09:54 -0500

    added sst config and hello world setup

---
+++ sst.config.ts
@@ -0,0 +1,14 @@
+import { SSTConfig } from "sst";
+import { API } from "./AWS/stacks/MyStack";
+
+export default {
+  config(_input) {
+    return {
+      name: "consignment-shop",
+      region: "us-east-2",
+    };
+  },
+  stacks(app) {
+    app.stack(API);
+  }
+} satisfies SSTConfig;
