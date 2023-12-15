381f98e96ef5b1262911c65740d1fc4805d18a69
Qui Nguyen <quinguyen.dev@gmail.com>
2023-12-15 08:38:59 -0500

    finished up buy option for computer

--- packages/web/app/routes/_app.$item.p.$id/route.tsx
+++ packages/web/app/routes/_app.$item.p.$id/route.tsx
@@ -1,9 +1,29 @@
-import { LoaderFunctionArgs } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import {
+  ActionFunctionArgs,
+  LoaderFunctionArgs,
+  redirect,
+} from "@remix-run/node";
+import { Form, useLoaderData } from "@remix-run/react";
+import axios from "axios";
+import { useState } from "react";
 import placeholderIcon from "~/assets/placeholder.svg";
 import { ItemCard, StoreCard } from "~/components";
 import { ComputerResultResponse, Store } from "~/hooks/types";
 import { useCustomerData } from "~/hooks/useCustomerData";
+
+export async function action({ request }: ActionFunctionArgs) {
+  const body = await request.formData();
+  const storeId = body.get("storeId");
+  const deviceId = body.get("deviceId");
+  const longitude = body.get("longitude");
+  const latitude = body.get("latitude");
+
+  await axios.post(
+    `customer/buy-device?deviceId=${deviceId}&custLatitude=${latitude}&custLongitude=${longitude}&storeId=${storeId}`,
+  );
+
+  return redirect("/");
+}
 
 export async function loader({ params }: LoaderFunctionArgs) {
   return { deviceName: params.item, deviceId: params.id };
@@ -12,25 +32,33 @@ export async function loader({ params }: LoaderFunctionArgs) {
 export default function ItemPage() {
   const data = useLoaderData<typeof loader>();
   const customerInfo = useCustomerData();
+  const [latitude, setLatitude] = useState("");
+  const [longitude, setLongitude] = useState("");
 
   const { data: query, isLoading: carouselIsLoading } =
     customerInfo.fetchHomePageData();
 
   const { data: device, isLoading } = customerInfo.fetchDevice(data.deviceId!);
 
-  if (isLoading && carouselIsLoading) return "Loading";
+  if ((isLoading && carouselIsLoading) || device == null) return "Loading";
+
+  const onChangeLatitude = (e: React.FormEvent<HTMLInputElement>) =>
+    setLatitude(e.currentTarget.value);
+
+  const onChangeLongitude = (e: React.FormEvent<HTMLInputElement>) =>
+    setLongitude(e.currentTarget.value);
 
   return (
     <>
-      <div className="flex space-x-12 py-6 px-12">
-        <div className="w-[566px] aspect-square flex justify-center items-center rounded-lg bg-gray-200 mb-4">
+      <div className="flex space-x-12 py-6 w-full px-4">
+        <div className="w-[700px] aspect-square flex justify-center items-center rounded-lg bg-gray-200 mb-4">
           <img src={placeholderIcon} alt="product image" />
         </div>
         <div className="w-full">
-          <h1 className="text-2xl font-bold">{device?.deviceName}</h1>
-          <h2 className="text-gray-400">{device?.storeName}</h2>
+          <h1 className="text-2xl font-bold">{device.deviceName}</h1>
+          <h2 className="text-gray-400">{device.storeName}</h2>
           <hr className="my-2" />
-          <div className="flex space-x-12 text-sm">
+          <div className="flex space-x-12">
             <div className="w-fit font-medium text-black">
               <p>Form factor: </p>
               <p>Processor manufacturer: </p>
@@ -44,24 +72,53 @@ export default function ItemPage() {
               <p>Operating system: </p>
             </div>
             <div className="w-fit text-gray-500">
-              <p>{device?.formFactor} </p>
-              <p>{device?.processorManufacturer}</p>
-              <p>{device?.processorModel}</p>
-              <p>{device?.memoryType}</p>
-              <p>{device?.memoryMb}</p>
-              <p>{device?.storageType}</p>
-              <p>{device?.storageGb}</p>
-              <p>{device?.gpuManufacturer}</p>
-              <p>{device?.gpuModel}</p>
-              <p>{device?.operatingSystem}</p>
+              <p>{device.formFactor} </p>
+              <p>{device.processorManufacturer}</p>
+              <p>{device.processorModel}</p>
+              <p>{device.memoryType}</p>
+              <p>{device.memoryMb}</p>
+              <p>{device.storageType}</p>
+              <p>{device.storageGb}</p>
+              <p>{device.gpuManufacturer}</p>
+              <p>{device.gpuModel}</p>
+              <p>{device.operatingSystem}</p>
             </div>
           </div>
         </div>
-        <div className="border w-[448px] p-4 flex-col space-y-4">
-          <p className="font-medium text-2xl">${device?.price}</p>
-          <button className="w-full p-2 bg-blue-500 text-white rounded-md">
-            Purchase
-          </button>
+        <div className="border rounded-md w-[448px] p-5 flex-col">
+          <p className="font-medium text-2xl">${device.price}</p>
+          <p className="text-sm font-medium mt-2">
+            Enter the following information to purchase the device
+          </p>
+          <Form method="post" className="text-sm space-y-2 mt-2">
+            <input type="hidden" name="deviceId" value={device.deviceId} />
+            <input type="hidden" name="storeId" value={device.storeId} />
+            <input
+              className="border p-1.5 w-full"
+              type="text"
+              name="longitude"
+              placeholder="Longitude"
+              onChange={(e) => onChangeLongitude(e)}
+            />
+            <input
+              className="border p-1.5 w-full"
+              type="text"
+              name="latitude"
+              placeholder="Latitude"
+              onChange={(e) => onChangeLatitude(e)}
+            />
+            <button
+              className={`w-full p-2 bg-blue-500 text-white rounded-md ${
+                longitude !== "" && latitude !== ""
+                  ? "opacity-100"
+                  : "opacity-50"
+              }`}
+              type="submit"
+              disabled={longitude === "" && latitude === ""}
+            >
+              Purchase
+            </button>
+          </Form>
         </div>
       </div>
       <section className="mt-6">
